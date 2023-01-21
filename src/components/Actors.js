7fd67b4fb89adcc001cc492523ec01a3b813ac64
tony555t <tonytullah764@gmail.com>
2023-01-21 14:48:13 +0300

    route

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,20 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
-}
-
-export default Actors;
+  const actorsInfo= actors.map ((actor) => (
+    <div key={actor.name}>
+      <h1> {actor.name} </h1>
+      <ul>
+        {actor.movies.map ((movie)=> (
+          <li key={movie}> {movie} </li>
+        ))}
+      </ul>
+    </div>
+  ))
+  return (
+    <div>
+      <h1>Actors Page</h1>
+      <div> {actorsInfo} </div>
+    </div>
+  )
+}export default Actors;
